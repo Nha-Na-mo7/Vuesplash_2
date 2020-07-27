40753585d20db519fabdd4f60adf057d776fbe6c
Nhala <zirconiump42@gmail.com>
2020-07-27 15:32:48 +0900

    second commit

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <main>
+      <div class="container">
+        <div><p>Appvue部分</p></div>
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
---
+++ resources/js/router.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// ページコンポーネントをインポートする
+
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterのプラグインを使用する
+// これによって、<RouterView />コンポーネントなどを使用できるようになる。
+Vue.use(VueRouter)
+
+
+
+// パスとコンポーネントのマッピング
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+// VueRouterのインスタンスを作成する
+const router = new VueRouter({
+  routes
+})
+
+// VueRouterインスタンスをエクスポートする。
+// app.jsでインポートするため。
+export default router;
